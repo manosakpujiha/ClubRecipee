0ef9108a9556dd7819c83b02ba7ff579fcf6d962
manos <manos.akpujiha@gmail.com>
2025-01-24 14:36:07 -0500

    Added MongoDB connection and recipe model and created first recipe

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const Recipe = require('./models/recipe');
 
+mongoose.connect('mongodb://127.0.0.1:27017/club-recipee')
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',  () => {
+    console.log('Connected to MongoDB');
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    // res.send('Welcome to Club Recipee Man');
     res.render('home');
+});
+
+app.get('/makerecipe', async (req, res) => {
+    const recipe = new Recipe({
+        title: 'Pancakes',
+        description: 'A delicious breakfast treat',
+        ingredients: '1 cup flour, 1 cup milk, 1 egg',
+        instructions: 'Mix ingredients, cook on griddle',
+        preptime: 10,
+        servings: 4
+    })
+    await recipe.save();
+    res.send('Recipe created!');
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+    title: String,
+    description : String,
+    ingredients: String,
+    instructions: String,
+    preptime: Number, // Preparation time in minutes
+    servings: Number, // Number of servings the recipe yields
+    // image: String
+});
+
+
+module.exports = mongoose.model('Recipe', RecipeSchema); // Export the model so that it can be used in other files
