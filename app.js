41f0cb56adc41d40a07d9509a353f9ebc51c2d54
manos <manos.akpujiha@gmail.com>
2025-01-24 13:17:01 -0500

    Added Vercel configuration and deployed app on vercel

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-// const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-    });
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    });
+    res.send('Welcome to Club Recipee');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+module.exports = app; 
