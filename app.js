8c2c65da8d13ce309758a2f9c5aaf08df83d3ce7
manos <manos.akpujiha@gmail.com>
2025-01-24 13:27:54 -0500

    Added EJS view engine and home page template

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('Welcome to Club Recipee Man');
+    // res.send('Welcome to Club Recipee Man');
+    res.render('home');
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
