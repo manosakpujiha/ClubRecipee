944a1d2fe9711fe9ddf0ed02b370268188ace746
manos <manos.akpujiha@gmail.com>
2025-01-24 21:55:47 -0500

    Updated MongoDB connection logic to use Vercel environment variable and enhance connection options

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const MONGODB_URI = process.env.NODE_ENV === 'production' 
+const MONGODB_URI = process.env.VERCEL_ENV === 'production' 
     ? process.env.MONGODB_URI_PROD 
     : 'mongodb://127.0.0.1:27017/club-recipee';
 
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 // const MONGODB_URI = 'mongodb+srv://vercel-admin-user:ed50OwKR4gNd8UFN@cluster0.0y17h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/club-recipee'
-mongoose.connect(`${MONGODB_URI}`);
+// mongoose.connect(`${MONGODB_URI}`);
 
 const db = mongoose.connection;
 
@@ -30,8 +34,8 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 app.get('/env', (req, res) => {
-    console.log(process.env.NODE_ENV);
-    res.send(process.env.NODE_ENV);
+    console.log(process.env);
+    res.send(process.env);
 });
 
 app.get('/recipes', async (req, res) => {
